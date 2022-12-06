7a096f20fcd6825df2d95e8112a62cf1e688c74d
wilkin17 <101288281+wilkin17@users.noreply.github.com>
2022-12-06 10:06:19 -0500

    Update PC_ROM_tb.v
    updated to support specific instr_out (same as out but need to be changed for datapath)

--- PC_ROM_tb.v
+++ PC_ROM_tb.v
@@ -1,11 +1,12 @@
-module PC_ROM_tb (next, current, rst, clk, rd, rs1, rs2, imm, out);
+module PC_ROM_tb (next, current, rst, clk, rd, rs1, rs2, imm, out, instr_out);
 output reg clk, rst;
 output [7:0] next, current;
-output [31:0] out;
+output [31:0] out, instr_out;
 output [4:0] rd, rs1, rs2;
 output [11:0] imm;
+//output [31:0] instr_out;
 
-PC_ROM dut (next, current, rst, clk, rd, rs1, rs2, imm, out);
+PC_ROM dut (next, current, rst, clk, rd, rs1, rs2, imm, out, instr_out);
 
 integer i;
 
